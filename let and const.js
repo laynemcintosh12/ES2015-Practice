9cf5e169a19c3f32ea0daf5bd0422fcfa3cf09a6
laynemcintosh12 <laynemcintosh12@gmail.com>
2023-05-12 18:36:48 -0400

    let and const

---
+++ let and const.js
@@ -0,0 +1,12 @@
+const pi = 3.14;
+
+// you can not redeclare a variable with let, you can with var. Let is a block scope Variable
+
+
+// you cant reassign or redeclare with const. const is a block scope variable
+
+
+// you can reassign variables with let
+
+
+// hoisting means that you can call on a variable before prior to it being listed and it will return undefined
